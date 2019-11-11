ad74915062cff66ff21b905882782576294807de
eastcloud10 <31729310+eastcloud10@users.noreply.github.com>
2019-11-11 21:29:53 +0800

    externalize
    数据独立化

--- auto_sub.py
+++ auto_sub.py
@@ -5,6 +5,8 @@ import cv2 as cv
 import time
 import ctypes
 import os
+import json
+from string import Template
 
 class TIME_it():
     def __init__(self):
@@ -14,7 +16,7 @@ class TIME_it():
         return time.time() - self.starttime
 class ACTOR():
     actor_list = []
-    def __init__(self,name='ray',fontname='ray字幕',defaulttext='【ray说：】',lowh=np.array([0,0,0]),uph=np.array([180,255,255]),kernelsize=5,start_amount=1200,end_amount=1000):
+    def __init__(self,name='ray',fontname='ray字幕',defaulttext='【ray说：】',lowh=np.array([0,0,0]),uph=np.array([180,255,255]),kernelsize=5,start_amount=1200,end_amount=1000,**kwargs):
         self.name = name
         self.lowh = lowh
         self.uph = uph
@@ -30,7 +32,7 @@ class ACTOR():
         self.defaulttext = defaulttext
         ACTOR.actor_list.append(self)
         
-    def compare_frames(self,frame_list,frame_count_of_alpha):
+    def rough_compare(self,frame_list,frame_count_of_alpha):
         criteria_dis,criteria_new = 0,0
         mask_omega = get_mask(frame_list[-1],self.lowh,self.uph,self.kernelsize)          
         mask_omega_sum = cv.countNonZero(mask_omega)
@@ -42,30 +44,12 @@ class ACTOR():
         if (mask_new_sum>self.start_amount) & (mask_new_sum/(mask_omega_sum+1) > 0.3):
             criteria_new = mask_new_sum//2
         if criteria_dis+criteria_new>0:
-            self.small_compare(frame_list,mask_omega,criteria_new,criteria_dis,frame_count_of_alpha+1)
+            self.deep_compare(frame_list,criteria_new,criteria_dis,frame_count_of_alpha+1)
         self.mask_alpha = mask_omega 
         self.mask_alpha_sum = mask_omega_sum
         return
-        
-    def dis_compare(self,frame_list,mask_omega, criteria_new=0, criteria_dis=0,frame_count_of_list0=1):
-        if len(frame_list) == 1:
-            for st in self.startframelist:
-                if frame_count_of_list0 - st >= 30:
-                    writetimestamp(ASS_FILENAME,FPS,st,frame_count_of_list0,self.fontname,self.defaulttext)
-            self.startframelist=[]
-        return
-        mid_frame = frame_list[(len(frame_list)-1)//2]
-        mask_mid = get_mask(mid_frame,self.lowh,self.uph,self.kernelsize)
-        if criteria_dis:
-            mask_dis = cv.bitwise_and(self.mask_alpha,cv.bitwise_xor(self.mask_alpha,mask_mid))
-            mask_dis_sum = cv.countNonZero(mask_dis)
-            if mask_dis_sum > criteria_dis:
-                self.small_compare(frame_list[:(len(frame_list)+1)//2],mask_omega,0,criteria_dis,frame_count_of_list0)
-            else:
-                self.small_compare(frame_list[(len(frame_list)+1)//2:],mask_omega,0,criteria_dis,frame_count_of_list0+(len(frame_list)+1)//2)
     
-    
-    def small_compare(self,frame_list,mask_omega, criteria_new=0, criteria_dis=0,frame_count_of_list0=1):
+    def deep_compare(self,frame_list, criteria_new=0, criteria_dis=0,frame_count_of_list0=1):
         if len(frame_list) == 1:
             if criteria_dis:
                 for st in self.startframelist:
@@ -84,20 +68,20 @@ class ACTOR():
             mask_dis = cv.bitwise_and(self.mask_alpha,cv.bitwise_xor(self.mask_alpha,mask_mid))
             mask_dis_sum = cv.countNonZero(mask_dis)
             if mask_dis_sum > criteria_dis:
-                self.small_compare(frame_list[:(len(frame_list)+1)//2],mask_omega,0,criteria_dis,frame_count_of_list0)
+                self.deep_compare(frame_list[:(len(frame_list)+1)//2],0,criteria_dis,frame_count_of_list0)
             else:
-                self.small_compare(frame_list[(len(frame_list)+1)//2:],mask_omega,0,criteria_dis,frame_count_of_list0+(len(frame_list)+1)//2)
+                self.deep_compare(frame_list[(len(frame_list)+1)//2:],0,criteria_dis,frame_count_of_list0+(len(frame_list)+1)//2)
         if criteria_new:
             mask_new = cv.bitwise_and(mask_mid,cv.bitwise_xor(mask_mid,self.mask_alpha))
             mask_new_sum = cv.countNonZero(mask_new)
             if mask_new_sum > criteria_new:
-                self.small_compare(frame_list[:(len(frame_list)+1)//2],mask_omega,criteria_new,0,frame_count_of_list0)
+                self.deep_compare(frame_list[:(len(frame_list)+1)//2],criteria_new,0,frame_count_of_list0)
             else:
-                self.small_compare(frame_list[(len(frame_list)+1)//2:],mask_omega,criteria_new,0,frame_count_of_list0+(len(frame_list)+1)//2)
+                self.deep_compare(frame_list[(len(frame_list)+1)//2:],criteria_new,0,frame_count_of_list0+(len(frame_list)+1)//2)
 
     def allend(self,frame_count):
         for st in self.startframelist:
-            writetimestamp(ASS_FILENAME,FPS,st,frame_count,self.name)
+            writetimestamp(ASS_FILENAME,FPS,st,frame_count,self.fontname,self.defaulttext)
         
 #根据范围取mask
 def get_mask(img,lowerhsv,upperhsv,kernelsize):
@@ -127,56 +111,20 @@ def frame_to_time(fc):
     return ("%d:%02d:%02d.%02d"%(hour,minute,second,centisecond))
 
 #初始化空ass文件
-def initial_ass(ASS_FILENAME,VIDEO_FILENAME):    
-    ASS_BASE="""[Script Info]
-; Script generated by Aegisub 3.2.2
-; http://www.aegisub.org/
-Title: New subtitles
-ScriptType: v4.00+
-WrapStyle: 0
-PlayResX: 1920
-PlayResY: 1080
-ScaledBorderAndShadow: yes
-YCbCr Matrix: TV.601
-
-[Aegisub Project Garbage]
-Last Style Storage: Default
-Audio File: %s
-Video File: %s
-Video AR Mode: 4
-Video AR Value: 1.777778
-Video Zoom Percent: 0.375000
-
-[V4+ Styles]
-Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
-Style: Default,Arial,45,&H00FFFFFF,&H000000FF,&H00000000,&H00000000,0,0,0,0,100,100,0,0,1,4.5,4.5,2,30,30,23,1
-Style: ray字幕,Microsoft YaHei UI,100,&H005D5EEF,&HFF0000FF,&H00FFFFFF,&H00000000,-1,0,0,0,100,100,0,0,1,6,0,2,10,10,220,1
-Style: rio字幕,Microsoft YaHei UI,100,&H00DE882B,&H000000FF,&H00FFFFFF,&H00000000,-1,0,0,0,100,100,0,0,1,6,0,2,10,10,220,1
-Style: 薄边框注释,Microsoft YaHei UI,60,&H00FFFFFF,&H000000FF,&H00000000,&H00000000,-1,0,0,0,100,100,0,0,1,3,0,2,10,10,10,1
-Style: 双色,Microsoft YaHei UI,100,&H005F4EE3,&H000000FF,&H00FFFFFF,&H00000000,-1,0,0,0,100,100,0,0,1,8,0,2,10,10,360,1
-Style: 边缘模糊注释,宋体,80,&H00FFFFFF,&H000000FF,&H00000000,&H00000000,-1,0,0,0,100,100,0,0,1,2,0,2,10,10,10,1
-Style: ray1通常,Microsoft YaHei UI,80,&H005D5EEF,&HFF0000FF,&H00FFFFFF,&H00000000,-1,0,0,0,100,100,0,0,1,4,0,2,10,10,10,1
-Style: rio1通常,Microsoft YaHei UI,80,&H00DE882B,&HFF0000FF,&H00FFFFFF,&H00000000,-1,0,0,0,100,100,0,0,1,4,0,2,10,10,10,1
-Style: 加厚边框注释,Microsoft YaHei UI,120,&H00FFFFFF,&H000000FF,&H00202020,&H00000000,-1,0,0,0,100,100,0,0,1,4,0,2,10,10,10,1
-
-[Events]
-Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
-Comment: 2,0:00:00.00,0:00:00.01,ray字幕,,0,0,0,template line keeptags,
-Comment: 1,0:00:00.00,0:00:00.01,ray字幕,,0,0,0,template line keeptags,{\\bord9\\3c&H5F4EE3&}
-Comment: 2,0:00:00.00,0:00:00.01,ray1通常,,0,0,0,template line keeptags,
-Comment: 1,0:00:00.00,0:00:00.01,ray1通常,,0,0,0,template line keeptags,{\\bord7\\3c&H5F4EE3&}
-Comment: 2,0:00:00.00,0:00:00.01,rio字幕,,0,0,0,template line keeptags,
-Comment: 1,0:00:00.00,0:00:00.01,rio字幕,,0,0,0,template line keeptags,{\\bord9\\3c&HD98936&}
-Comment: 2,0:00:00.00,0:00:00.01,rio1通常,,0,0,0,template line keeptags,
-Comment: 1,0:00:00.00,0:00:00.01,rio1通常,,0,0,0,template line keeptags,{\\bord7\\3c&HD98936&}
-Comment: 2,0:00:00.00,0:00:00.01,双色,,0,0,0,template line keeptags,{\\pos($sx,$sy)\\clip(!$lleft-20!,!$ltop-20!,!$lright+20!,$lmiddle)}
-Comment: 1,0:00:00.00,0:00:00.01,双色,,0,0,0,template line keeptags,{\\pos($sx,$sy)\\bord12\\3c&H5F4EE3&\\clip(!$lleft-20!,!$ltop-20!,!$lright+20!,$lmiddle)}
-Comment: 2,0:00:00.00,0:00:00.01,双色,,0,0,0,template line keeptags,{\\pos($sx,$sy)\\1c&HD98936&\\clip(!$lleft-20!,$lmiddle,!$lright+20!,!$lbottom+20!)}
-Comment: 1,0:00:00.00,0:00:00.01,双色,,0,0,0,template line keeptags,{\\pos($sx,$sy)\\bord12\\3c&HD98936&\\1c&HD98936&\\clip(!$lleft-20!,$lmiddle,!$lright+20!,!$lbottom+20!)}
-"""%(VIDEO_FILENAME,VIDEO_FILENAME)
+def initial_ass():
+    print(0)
+    with open("空模版.ass","r",encoding='utf-8') as f:
+        emptyread= Template(f.read())
+    ASS_BASE = emptyread.substitute(aWIDTH=str(WIDTH),aHEIGHT=str(HEIGHT),aVIDEO_FILENAME = VIDEO_FILENAME)
     with open(ASS_FILENAME,"w",encoding='utf-8') as f:
         f.write(u'\ufeff') #防Aegisub乱码
         f.write(ASS_BASE)
+        
+#向ass中写入时间轴数据
+def writetimestamp(ASS_FILENAME,FPS,startframe,endframe,fontname,defaulttext):
+    with open(ASS_FILENAME,'a',encoding="utf-8") as f:
+        f.write("\nDialogue: 0,%s,%s,%s,,0,0,0,,%s"%(frame_to_time(startframe),frame_to_time(endframe),fontname,defaulttext))
+        
 def progress_bar(frame_count):
     totaltime = clock.tick()
     print('')
@@ -192,10 +140,7 @@ def progress_bar(frame_count):
     print("预计剩余时间：%d分%d秒"%(time_left/60,time_left%60))
     print("--------") #进度条
     
-#向ass中写入时间轴数据。样式为ray字幕和rio字幕
-def writetimestamp(ASS_FILENAME,FPS,startframe,endframe,fontname,defaulttext):
-    with open(ASS_FILENAME,'a',encoding="utf-8") as f:
-        f.write("Dialogue: 0,%s,%s,%s,,0,0,0,,%s\n"%(frame_to_time(startframe),frame_to_time(endframe),fontname,defaulttext))
+
  
 if __name__ == "__main__": 
     #修改终端标题
@@ -213,8 +158,8 @@ if __name__ == "__main__":
             break
     else:
         VIDEO_FILENAME = input('请输入视频文件名（含扩展名）：\n')
-    ASS_FILENAME = "【自动生成】"+VIDEO_FILENAME[:-4]+'.ass'
-    initial_ass(ASS_FILENAME,VIDEO_FILENAME)
+    
+    
 
     #载入视频
     cap = cv.VideoCapture(VIDEO_FILENAME,cv.CAP_FFMPEG) #打开视频
@@ -225,11 +170,19 @@ if __name__ == "__main__":
     WIDTH = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
     HEIGHT = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))    
 
+    ASS_FILENAME = "【自动生成】"+VIDEO_FILENAME[:-4]+'.ass'
+    initial_ass()
+    
     #字型列表
-    RAY = ACTOR(name='ray',fontname='ray字幕',defaulttext='【ray说：】',lowh=np.array([174,163,215]),uph=np.array([180,170,245]),kernelsize=5,start_amount=2000,end_amount=1400)
-    RIO = ACTOR(name='rio',fontname='rio字幕',defaulttext='【rio说：】',lowh=np.array([100,170,188]),uph=np.array([105,210,217]),kernelsize=5,start_amount=2000,end_amount=1400)
-    BLACK = ACTOR(name='BLACK',fontname='加粗边框注释',defaulttext=r'{\bord8}【加粗边框注释】',lowh=np.array([0,0,14]),uph=np.array([179,40,46]),kernelsize=3,start_amount=9000,end_amount=6300)
-    GRAY = ACTOR(name='GRAY',fontname='边缘模糊注释',defaulttext=r'{\blur5}【边缘模糊文字】',lowh=np.array([0,0,100]),uph=np.array([179,20,131]),kernelsize=3,start_amount=10000,end_amount=7000)
+    with open("样式列表.json","r",encoding='utf-8') as f:
+        actorjson=f.read()
+    jsactorlist=json.loads(actorjson)
+    for jsactor in jsactorlist:
+        ACTOR(**jsactor,uph=np.array(jsactor["uh"]),lowh=np.array(jsactor["lh"]))
+    #RAY = ACTOR(name='ray',fontname='ray字幕',defaulttext='【ray说：】',lowh=np.array([174,163,215]),uph=np.array([180,170,245]),kernelsize=5,start_amount=2000,end_amount=1400)
+    #RIO = ACTOR(name='rio',fontname='rio字幕',defaulttext='【rio说：】',lowh=np.array([100,170,188]),uph=np.array([105,210,217]),kernelsize=5,start_amount=2000,end_amount=1400)
+    #BLACK = ACTOR(name='BLACK',fontname='加粗边框注释',defaulttext=r'{\bord8}【加粗边框注释】',lowh=np.array([0,0,14]),uph=np.array([179,40,46]),kernelsize=3,start_amount=9000,end_amount=6300)
+    #GRAY = ACTOR(name='GRAY',fontname='边缘模糊注释',defaulttext=r'{\blur5}【边缘模糊文字】',lowh=np.array([0,0,100]),uph=np.array([179,20,131]),kernelsize=3,start_amount=10000,end_amount=7000)
     
     #进度条
     print("----------")
@@ -247,7 +200,7 @@ if __name__ == "__main__":
         period_frames.append(img)
         if frame_count%SERIES_LENGTH == SERIES_LENGTH-1:
             for actor in ACTOR.actor_list:
-                actor.compare_frames(period_frames,alpha_frame_count)
+                actor.rough_compare(period_frames,alpha_frame_count)
             alpha_frame_count = frame_count
             period_frames = []
             print('|',end='',flush=True)    
@@ -265,4 +218,3 @@ if __name__ == "__main__":
     #释放资源
     cap.release()
     cv.destroyAllWindows()
-
